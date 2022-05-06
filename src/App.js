0360db88b3e6905a58607c2bd5a3e7fdee283db6
satania-lee <rockky.tl@gmail.com>
2022-05-07 00:09:15 +0300

    добовлен Post

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Post } from './components/Post'
 
 export const App = () => {
   return (
-    <div>App_test</div>
+    <div className='appContainer'>
+    <Routes>
+      <Route path='posts/:postID' element={<Post />}/>
+    </Routes>
+    </div>
   )
 }
